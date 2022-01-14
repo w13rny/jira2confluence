a99335563ca4f5058a82e9231f23f9504c40df37
w13rny <marcin.wiera@gmail.com>
2022-01-14 09:52:18 +0100

    Jira links as macros on Confluence page - closes #3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import time
 
 from atlassian import Confluence
@@ -7,11 +8,21 @@ import config as cfg
 from models.page import Page
 
 
-def fix_newlines(confluence_page_obj: dict) -> str:
-    body = confluence_page_obj['body']['storage']['value']
+def fix_newlines(body: str) -> str:
     body = body.replace('<br />\n', '\n')
     body = body.replace('\t', '')
     return body
+
+
+def jira_links_to_macros(body: str) -> str:
+    jira_link = cfg.jira['url'] + '/browse/'
+    jira_link = jira_link.replace('/', '\/')
+    regex = '(<a href="' + jira_link + '[^>]*>)(' + jira_link + ')([^<]*)(<\/a>)'
+    result = re.sub(
+        regex,
+        f"{cfg.confluence['jira_macro_prefix']}\\3{cfg.confluence['jira_macro_suffix']}",
+        body)
+    return result
 
 
 if __name__ == '__main__':
@@ -45,7 +56,9 @@ if __name__ == '__main__':
 
     for page in cfg.pages:
         page_obj = confluence.get_page_by_id(page['page_id'], expand='body.storage')
-        new_body = fix_newlines(page_obj)
+        new_body = page_obj['body']['storage']['value']
+        new_body = fix_newlines(new_body)
+        new_body = jira_links_to_macros(new_body)
         confluence.update_page(
             page_id=page['page_id'],
             title=page['page_title'],
@@ -54,4 +67,4 @@ if __name__ == '__main__':
             representation='storage',
             minor_edit=False
         )
-        print(page['page_title'] + ' - na stronie zostały naprawione znaki nowej linii.')
+        print(page['page_title'] + ' - strona została skorygowana (znaki nowej linii i marka linków do Jiry).')
